052dd944c9110261a0e8098adb514dc22cf90c00
Garth Henson <garth@guahanweb.com>
2018-02-05 15:36:35 -0500

    Setting up first responsive styles

--- header.php
+++ header.php
@@ -1,11 +1,8 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo('charset'); ?>">
-<meta name="viewport" content="widt=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 <link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600" media="screen">
 <link type="text/css" rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.6/css/all.css" media="screen">

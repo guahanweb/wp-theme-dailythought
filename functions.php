07d3dcd94dde93081b154cee65f3b80173422f31
Garth Henson <garth@guahanweb.com>
2018-03-09 21:37:53 -0500

    menus

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+function register_theme_menus() {
+    register_nav_menus(
+        array(
+            'header_menu' => __('Header Menu'),
+            'secondary_menu' => __('Secondary Menu')
+        )
+    );
+}
+add_action('init', 'register_theme_menus');
+?>

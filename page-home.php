55f1aa3f2a6d07daac596fa88f241a8463473290
Garth Henson <garth@guahanweb.com>
2018-03-31 10:27:20 -0400

    Remove placeholder content until page is done

--- page-home.php
+++ page-home.php
@@ -24,11 +24,13 @@ get_header();
 echo '<main class="page-content">';
 if (have_posts()):
     while(have_posts()): the_post();
+        /*
         echo '<div class="container">';
             echo '<div class="main-content">';
                 the_content();
             echo '</div>';
         echo '</div>';
+        */
     endwhile;
 endif;
 

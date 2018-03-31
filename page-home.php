de3e76d7500e0c3a764a819f7ba08a0034f3adfb
Garth Henson <garth@guahanweb.com>
2018-03-31 08:49:35 -0400

    Placeholder for home page content

--- page-home.php
+++ page-home.php
@@ -12,12 +12,27 @@
  * @since GWDailyThought 1.0
  */
 
+// get last 10 thoughts to share
+$thoughts = get_posts(array(
+    'numberposts' => 10,
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'post_type' => 'thought'
+));
+
 get_header();
 if (have_posts()):
     while(have_posts()): the_post();
         the_content();
     endwhile;
 endif;
+
+if ($thoughts) {
+    foreach ($thoughts as $post) {
+        get_template_part('content', get_post_type());
+    }
+}
+
 get_footer();
 ?>
 
